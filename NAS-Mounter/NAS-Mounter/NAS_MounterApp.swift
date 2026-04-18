a6769af529ae6feb87ac91ab3d4197142db429cd
josueluna <josue.luna.gamboa@gmail.com>
2026-04-17 19:13:11 -0600

    feature: added connection logic to keychain

--- NAS-Mounter/NAS-Mounter/NAS_MounterApp.swift
+++ NAS-Mounter/NAS-Mounter/NAS_MounterApp.swift
@@ -1,16 +1,12 @@
 import SwiftUI
 
 @main
-struct NAS_MounterApp: App {
+struct NASMounterApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(width: 380, height: 260)
-                .onAppear {
-                    if let window = NSApplication.shared.windows.first {
-                        window.title = "NAS Mounter"  // Establece el título de la ventana
-                    }
-                }
         }
+        .windowResizability(.contentSize)
+        .defaultSize(width: 380, height: 1)
     }
 }
